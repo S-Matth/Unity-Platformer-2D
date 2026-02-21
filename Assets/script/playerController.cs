5d355afe61eb90563f6ebac29f6ee2605a16967e
S-Matth <matthias.smidts@student.helb-prigogine.be>
2026-02-21 18:15:23 +0100

    modif-jump
    add wall jump and dubble jump

--- Assets/script/playerController.cs
+++ Assets/script/playerController.cs
@@ -8,9 +8,10 @@ public class playerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 2.5f;
     [SerializeField] private float wallSlidingSpeed = 0.5f;
     [SerializeField] private float jumpForce = 9f;
-    [SerializeField] private Vector2 wallJumpingPower = new Vector2(8f, 16f);
     [SerializeField] private LayerMask lmGround;
     [SerializeField] private LayerMask lmWall;
+
+
 
     // Variable Move
     private Rigidbody2D rb;
@@ -21,18 +22,19 @@ public class playerController : MonoBehaviour
     private SpriteRenderer sp;
     private bool isGrounded;
     private Transform groundCheck;
+    private float maxJump = 2;
+    private float cptJump = 0;
 
     // Variable wallSliding
     private bool isWallSliding;
     private Transform wallCheck;
 
-    // Variable wallJumping
+    // Variable wallJump
+    private float wallJumpTimer;
+    private float wallJumpForce = 4.5f;
+    private Vector2 wallJumpDirection = new Vector2(1f, 1.5f);
+    private float wallJumpDuration = 0.2f;
     private bool isWallJumping;
-    private float wallJumpingDirection;
-    private float wallJumpingCounter;
-    private float wallJumpingTime = 0.2f;
-    private float wallJumpingDuration = 0.4f;
-    private bool isJumping = false;
 
     private Animator animator;
     
@@ -50,7 +52,7 @@ public class playerController : MonoBehaviour
     private void Update()
     {
         // Sens du personnage
-        if (!isWallJumping) OrientationCharactere();
+        OrientationCharactere();
 
         // Est ce que le personnage est au sol ?
         IsGrounded();
@@ -61,22 +63,33 @@ public class playerController : MonoBehaviour
 
         // Gestion du wall slide
         WallSlide();
-        //WallJump();
+        HandleWallJumpTimer();
     }
 
     private void FixedUpdate()
     {
-        if (!isWallJumping) rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocityY);
+        if(!isWallJumping) rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocityY);
 
     }
 
 
-    private void IsGrounded() => isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, lmGround);
+    private void IsGrounded()
+    {
+        bool wasGrounded = isGrounded;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, lmGround);
+        if (isGrounded && !wasGrounded) cptJump = 0;
+    }
 
-    private bool IsWalled() => Physics2D.OverlapCircle(wallCheck.position, 0.1f, lmWall);
+    private bool IsWalled() => Physics2D.OverlapCircle(wallCheck.position, 0.05f, lmWall);
 
     private void WallSlide()
     {
+        if (isWallJumping)
+        {
+            isWallSliding = false;
+            return;
+        }
+
         if (IsWalled() && !isGrounded && moveInput.x != 0)
         {
             isWallSliding = true;
@@ -87,6 +100,19 @@ public class playerController : MonoBehaviour
             isWallSliding = false;
         }
     }
+
+    private void HandleWallJumpTimer()
+    {
+        if (isWallJumping)
+        {
+            wallJumpTimer -= Time.deltaTime;
+            if (wallJumpTimer <= 0f)
+            {
+                isWallJumping = false;
+            }
+        }
+    }
+
     private void GestionAnimation()
     {
         // Animation Move
@@ -102,50 +128,46 @@ public class playerController : MonoBehaviour
         if (moveInput.x != 0) lastXDirection = Mathf.Sign(moveInput.x);
         sp.flipX = (lastXDirection < 0);
 
-        wallCheck.localPosition = new Vector3(0.1f * lastXDirection, wallCheck.localPosition.y, 0);
+        wallCheck.localPosition = new Vector3(0.05f * lastXDirection, wallCheck.localPosition.y, 0);
     }
-
-        
-        
-
 
     private void WallJump()
     {
-        if (isWallSliding)
-        {
-            isWallJumping = false;
-            wallJumpingDirection = -moveInput.x;
-            wallJumpingCounter = wallJumpingTime;
-            CancelInvoke(nameof(StopWallJumping));
-        }
-        else
-        {
-            wallJumpingCounter -= Time.deltaTime;
-        }
-        if (isJumping && wallJumpingCounter > 0f)
-        {
-            isWallJumping = true;
-            rb.linearVelocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
-            wallJumpingCounter = 0f;
+        isWallSliding = false ;
+        isWallJumping = true;
+        wallJumpTimer = wallJumpDuration;
 
-            if (moveInput.x != wallJumpingDirection) OrientationCharactere();
+        // direction opposée au mur
+        float jumpDirection = -lastXDirection;
 
-            Invoke(nameof(StopWallJumping), wallJumpingDuration);
-        }
+        rb.linearVelocity = new Vector2(
+            wallJumpDirection.x * jumpDirection * wallJumpForce,
+            wallJumpDirection.y * wallJumpForce);
     }
 
-    private void StopWallJumping() => isWallJumping = false;
     private void OnMove(InputValue value) => moveInput = value.Get<Vector2>();
 
     private void OnJump(InputValue value)
     {
         if (value.isPressed)
         {
-            isJumping = true;
-            if(isGrounded) rb.linearVelocityY = jumpForce;
-        }
-        else isJumping = false;
-            
+            if (isGrounded)
+            {
+                rb.linearVelocityY = jumpForce;
+                cptJump++;
+            }
+
+            else if (!isGrounded && !IsWalled() && maxJump > cptJump)
+            {
+                rb.linearVelocityY = jumpForce;
+                cptJump++;
+            }
+            else if (IsWalled())
+            {
+                cptJump = 2;
+                WallJump();
+            }
+        }   
     }
 
     
