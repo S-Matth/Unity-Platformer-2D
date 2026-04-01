af32215332ee8e9481dd9edc4ab59e57b946bec9
JasonDang77 <jason.dang@student.helb-prigogine.be>
2026-04-01 16:48:02 +0200

    Add prefabs "panneau"
    j'ai ajouté un préfabs a drag and drop pour le panneau. changer le texte sur panneau. appuie sur E pour lire le panneau

---
+++ Assets/script/Jscript/panneau.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+
+public class Panneau : MonoBehaviour
+{
+    public GameObject panelTexte;
+    public TextMeshProUGUI texteUI;
+
+    [TextArea]
+    public string message;
+
+    private bool joueurProche = false;
+
+    void Start()
+    {
+        panelTexte.SetActive(false);
+        texteUI.text = message;
+    }
+
+    void Update()
+    {
+        if (joueurProche && Input.GetKeyDown(KeyCode.E))
+        {
+            panelTexte.SetActive(!panelTexte.activeSelf);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            joueurProche = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            joueurProche = false;
+            panelTexte.SetActive(false);
+        }
+    }
+}
