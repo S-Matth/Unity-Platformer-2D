9dd9b262f7ea670f711f421e6f136666d01fd076
CharlesPan <charles.pereira@student.helb-prigogine.be>
2026-04-06 14:20:07 +0200

    charles 06/04
    prefab SceneManager

--- Assets/script/CchangeScene.cs
+++ Assets/script/CchangeScene.cs
@@ -2,9 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /*
-    Vous devez creer un objet vide (ou ajouter un autre objet) dans la hierarchie, ajouté un boxcollider2D et ce script.
-    Dans les parametres du script vous allez voir l'index, vous devez placer le numero de la scene a laquelle vous voulez passer
-    pour voir le numero de la scenes, vous devez ouvrir en haut a droit File -> Build Profiles. Suivez l'ordre. Si Vous etes numero 1 choisissez numero 2
+    ne touchez pas au script ni au changement de scene, placé le prefab à l'endroit qui voulais et c'est tout
 */
 public class CchangeScene : MonoBehaviour
 {
