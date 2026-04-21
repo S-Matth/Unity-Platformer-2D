00753ff44c14cc1e56d5aa14f67bc7a4e242d311
VictorGuedouar <victor.guedouar@student.helb-prigogine.be>
2026-04-21 10:18:25 +0200

    up
    up

--- Assets/script/Vscript/SettingsMenu.cs
+++ Assets/script/Vscript/SettingsMenu.cs
@@ -14,13 +14,13 @@ public class SettingsMenu : MonoBehaviour
 
     public void Start()
     {
-        resolutions = Screen.resolutions.Select(resolution  => new Resolution { width= resolution.width, height = resolution.height}) .Distinct().ToArray();
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
 
         int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++) 
+        for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
